b20a3e19cc107fce4c4100c2b5a5abe8460b0dac
Flavio Nunes <flavionbueri@gmail.com>
2023-01-10 02:31:23 -0300

    Implementando delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+	Scanner sc = new Scanner(System.in);
 	SellerDao sellerDao = DaoFactory.createSellerDao();
 	
 	System.out.println("=== TESTE 1 - findById ===");
@@ -49,7 +51,19 @@ public class Program {
     seller.setName("Martha Fushia");
     sellerDao.update(seller);
     System.out.println("Atualização completa!");
+
+    System.out.println("-----------");
+    System.out.println("=== TESTE 6 - sellerDelete ===");
+    
+    System.out.print("Selecione o Id para ser deletado: ");
+     int id = sc.nextInt();
+    
+    sellerDao.deleteById(id);
+    
+    System.out.println("Exclusão completa!");
+    		 
+    		 sc.close();
 	}
 	
-
+	
 }
