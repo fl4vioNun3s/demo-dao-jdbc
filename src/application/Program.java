ab923d028a78b1af0ff768fa7a477791792e9de2
Flavio Nunes <flavionbueri@gmail.com>
2023-01-09 23:30:15 -0300

    Implementando a classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,12 +9,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Tchelvs");
-		System.out.println(obj);
-		System.out.println();
-		
-		Seller seller = new Seller(21,"Bob", "tchlvs@gmail.com", LocalDate.now(), 3000.0, obj);
-		System.out.println(seller);
+		System.out.println("Teste");
 		
 		
 
